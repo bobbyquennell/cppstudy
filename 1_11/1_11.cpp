4a21f9f6b555fbde43e1295069e2c4435ec9a65a
WANG <bobbyquennell@gmail.com>
2013-01-20 13:10:04 +0800

    set up my c++ study project

---
+++ 1_11/1_11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+int main()
+{
+    int i = 10;
+/*
+    while (i >=0)
+    {
+		std::cout << i << std::endl;
+	    i--;
+    }
+	return 0;
+*/
+	for (int a = 10; a >=0; a--)
+	{
+		std::cout << a << std::endl;
+	}
+	return 0;
+}
